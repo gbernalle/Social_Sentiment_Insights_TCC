b4a72d812c9553705002df3491532f1c1c33aefc
gbernalle <gabrielbbmatozinhos@gmail.com>
2025-10-20 14:59:55 -0300

    Informacoes do reddit incluidas, mas está errado. Apagar a conta quando a AWS voltar, criar outra para ter nome de usuario ok, e gerar os tokens de novo

--- reddit_connection.py
+++ reddit_connection.py
@@ -1,1 +1,23 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path='keyword.env')
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+password = os.getenv('PASSWORD')
+
+auth = requests.auth.HTTPBasicAuth(client_id,client_secret)
+data = {
+  'grant_type': 'password',
+  'username': 'u/One_Competition4014',
+  'password': password
+}
+
+headers = {'User-Agent':'TccTeste/0.0.1'}
+
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+
+res.json()
