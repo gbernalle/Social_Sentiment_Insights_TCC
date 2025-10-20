2ee79fee4cb09412a9df1078d6e6347c7dd27762
gbernalle <gabrielbbmatozinhos@gmail.com>
2025-10-20 16:04:36 -0300

    Forma de fazer o request na unha, mas o ideal é com praw, que farei agora

--- reddit_connection.py
+++ reddit_connection.py
@@ -2,22 +2,25 @@ import requests
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv(dotenv_path='keyword.env')
 client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+secret_key = os.getenv('SECRET_KEY')
 password = os.getenv('PASSWORD')
+user_name = os.getenv('USER_REDDIT')
 
-auth = requests.auth.HTTPBasicAuth(client_id,client_secret)
+auth = requests.auth.HTTPBasicAuth(client_id, secret_key)
 data = {
   'grant_type': 'password',
-  'username': 'u/One_Competition4014',
+  'username': user_name,
   'password': password
 }
 
 headers = {'User-Agent':'TccTeste/0.0.1'}
 
 res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
+                    auth=auth, data=data, headers=headers, verify=False)
 
-res.json()
+TOKEN = res.json()['access_token']
+headers['Authorization'] = f'bearer {TOKEN}'
+
+requests.get('https://oauth.reddit.com/api/v1/me',headers=headers, verify=False).json()
